8cedfd64010e3cc968c7c4d0cf528402cb7d312c
Harish Subramaniam <harry9.11.1985@gmail.com>
2016-11-30 20:13:08 +1100

    Committing new version of InboxPage Test functionality and maven integration with TestNG

---
+++ SeleniumGmailProject/src/com/testng/gmail/tests/TestGmailInboxPage.java
@@ -0,0 +1,50 @@
+package com.testng.gmail.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.gmail.pageobjects.GmailInboxPage;
+import com.gmail.pageobjects.GmailLoginPage;
+import com.selenium.ex.util.io.DataRow;
+import com.testng.gmail.tests.dataproviders.GmailDataProviderSource;
+
+public class TestGmailInboxPage {
+
+	
+	WebDriver driver;
+	GmailLoginPage loginPage;
+	GmailInboxPage inboxPage;
+	
+	@BeforeClass(alwaysRun=true)
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver", "C:\\Harish Software\\selenium\\chromedriver_win32\\chromedriver.exe");
+	}
+	
+	@BeforeMethod(alwaysRun=true)
+	public void loginSuccessfullyToGmail(){
+		driver = new ChromeDriver();
+	}
+	
+	@Test(groups = {"functest"} ,dataProvider="loginSuccessData" , dataProviderClass=GmailDataProviderSource.class)
+	public void testComposeEmail(DataRow dataRow){
+		loginPage = GmailLoginPage.newInstance(driver, dataRow.getAttributeValue("Email"), dataRow.getAttributeValue("Password"));
+		loginPage.performLoginSuccess();
+	    inboxPage = GmailInboxPage.newInstance(driver);
+		boolean isMailComposed = inboxPage.performComposeEmail("Test Email from Selenium ",dataRow.getAttributeValue("Email"));
+	   	if(isMailComposed){
+			Assert.assertTrue(inboxPage.verifyEmailComposed("Test Email from Selenium ",dataRow.getAttributeValue("Email")));
+	   	}
+	}
+	
+	@AfterMethod(alwaysRun=true)
+	public void cleanUp(){
+		driver.quit();
+		driver = null;
+	}
+	
+}
